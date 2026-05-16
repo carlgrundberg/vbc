12f8d008245d5c63caa03370f2dab24cd1e48567
Carl Grundberg <carl.grundberg@menmo.se>
2026-05-16 12:12:37 +0200

    chore: update Next.js configuration for local image patterns and upgrade package dependencies to latest versions for improved stability

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,9 @@ import { withPayload } from '@payloadcms/next/withPayload'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config here
+  images: {
+    localPatterns: [{ pathname: '/api/media/file/**' }],
+  },
 }
 
 export default withPayload(nextConfig)
